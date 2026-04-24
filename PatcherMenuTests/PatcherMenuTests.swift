0fa6cff83c03295637d9fac1bc57ded6da095b2c
Gil Burns <gil@gilburns.com>
2026-04-23 22:52:55 -0500

    PatcherMenuTests code files

---
+++ PatcherMenuTests/PatcherMenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  PatcherMenuTests.swift
+//  PatcherMenuTests
+//
+//  Created by Gil Burns on 4/22/26.
+//
+
+import Testing
+@testable import PatcherMenu
+
+struct PatcherMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
