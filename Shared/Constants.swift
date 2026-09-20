8f2091ab9136741542676903eeb200b5cd5f3897
Gil Burns <gil@gilburns.com>
2026-09-20 11:39:54 -0500

    Rev the version for new release
    Rev the version for new release. Now 1.2.1

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -13,7 +13,7 @@ struct AppConstants {
     static let randomGUID = UUID().uuidString
 
     static let patcherFullName = "Third Party Patcher"
-    static let patcherVersion = "1.2.0"
+    static let patcherVersion = "1.2.1"
 
     // MARK: - Binary paths
 
